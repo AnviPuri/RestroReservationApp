249dece2bdecb0de8f23e7158676fdea32f378f3
Anvi Puri <anvipuri@gmail.com>
2019-10-16 22:45:44 +0530

    reservationController change

--- app-presentation/src/main/java/controller/ReservationController.java
+++ app-presentation/src/main/java/controller/ReservationController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,15 @@ import data.entities.ReservationRequest;
 @RestController
 public class ReservationController extends BaseController {
 	
+	@PostMapping
 	public void makeReservation(@RequestBody ReservationRequest reservationRequest) {
 		
 	}
+	
+	@GetMapping
+	public void getReservation(@RequestBody ReservationRequest reservationRequest) {
+		
+	}
+	
 
 }
